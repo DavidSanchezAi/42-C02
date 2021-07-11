9364bdc424d6f5d9413170d595f844a9e9993510
David Sanchez rodriguez <dasanche@c3r11s1.42madrid.com>
2021-07-11 10:43:15 +0200

    ex01 yex10 add

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,28 +6,17 @@
 /*   By: dasanche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 12:21:37 by dasanche          #+#    #+#             */
-/*   Updated: 2021/07/06 13:13:01 by dasanche         ###   ########.fr       */
+/*   Updated: 2021/07/08 13:09:58 by dasanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	unsigned int	cont;
-	int				i;
-	int				j;
+	int	i;
 
-	i = 0;
-	j = 0;
-	size++;
-	cont = size;
-	while (cont > 1)
-	{
-		dest [i] = src [i];
-		i++;
-		cont--;
-	}
-	dest [i] = '\0';
-	while (src [j] != '\0')
-		j++;
-	return (j);
+	i = -1;
+	while (src[++i] != '\0' && i < size - 1)
+		dest[i] = src[i];
+	dest[i++] = '\0';
+	return (size - i);
 }
